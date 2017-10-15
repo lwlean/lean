504c05f8d9be69ff21dd74a9dec594f8f7644a35
DESKTOP-OKT71NA\llw <lwlean@outlook.com>
2017-10-15 23:03:03 +0800

    增加创建user sql文件

---
+++ src/main/resources/sql/users.sql
@@ -0,0 +1,10 @@
+CREATE SEQUENCE user_id_seq START WITH 1 INCREMENT BY 1 MINVALUE 1 NO MAXVALUE;
+
+CREATE TABLE "public"."users" (
+  "id" int8 DEFAULT nextval('user_id_seq'::regclass) NOT NULL,
+  "name" varchar(255) COLLATE "default",
+  "password" varchar(255) COLLATE "default",
+  "age" int4,
+  "introduction" varchar(255) COLLATE "default"
+)
+

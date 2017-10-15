ed5f55990384a63b71a46462336cad98a6ec64eb
DESKTOP-OKT71NA\llw <lwlean@outlook.com>
2017-10-15 23:03:45 +0800

    修改sql文件

--- src/main/resources/sql/users.sql
+++ src/main/resources/sql/users.sql
@@ -2,9 +2,9 @@ CREATE SEQUENCE user_id_seq START WITH 1 INCREMENT BY 1 MINVALUE 1 NO MAXVALUE;
 
 CREATE TABLE "public"."users" (
   "id" int8 DEFAULT nextval('user_id_seq'::regclass) NOT NULL,
-  "name" varchar(255) COLLATE "default",
-  "password" varchar(255) COLLATE "default",
+  "name" varchar(255),
+  "password" varchar(255),
   "age" int4,
-  "introduction" varchar(255) COLLATE "default"
+  "introduction" varchar(255)
 )
 
